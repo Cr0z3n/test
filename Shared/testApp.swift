b950eaf757fece398dda76b9976247fd7cef1736
Paul <paul.doussot@icloud.com>
2021-04-22 09:04:24 +0200

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Paul on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
